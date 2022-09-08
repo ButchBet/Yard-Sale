d167c0da7cb014de57ef72d0a00c30534393d3ed
ButchBet <butchbet01@gmail.com>
2022-09-07 21:56:29 -0500

    Correcting some style issues and changed the structure of the main.js document.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,39 @@
-// Script to open and close the desktop and mobile menu 
 const switchMenus = Array.from(document.getElementsByClassName('menu'));
 const desktopMenu = document.getElementById('desktopMenu');
 const mobileMenu = document.getElementById('mobileMenu');
+const switchShoppingCarts = Array.from(document.getElementsByClassName('switchShoppingCart'));
+const shoppingCart = document.getElementById('shoppingCart');
+const numberOfItems = Array.from(document.getElementsByClassName('numberOfItems'));
+const emailMobileMenu = document.getElementById('emailMobileMenu');
 
+// Script to open and close the desktop and mobile menu 
 switchMenus.forEach((menu) => {
     menu.addEventListener('click', (e) => {
         e.preventDefault();
         desktopMenu.classList.toggle('hidden');
         mobileMenu.classList.toggle('hidden');
+        shoppingCart.classList.add('hidden');
     });
 })
 
 // Script to correct reactions
-const emailMobileMenu = document.getElementById('emailMobileMenu');
-
 emailMobileMenu.addEventListener('click', (e) => {
     e.preventDefault();
 })
 
-
 // Script to open and close the shopping cart
-const switchShoppingCarts = Array.from(document.getElementsByClassName('switchShoppingCart'));
-const shoppingCart = document.getElementById('shoppingCart');
-
 switchShoppingCarts.forEach((switchShoppingCart) => {
     switchShoppingCart.addEventListener('click', (e) => {
         shoppingCart.classList.toggle('hidden');
+        desktopMenu.classList.add('hidden');
+        mobileMenu.classList.add('hidden');
     });
 })
 
 // Script to add the number pf items added in the shopping cart in the shopping cart icon
-const numberOfItems = Array.from(document.getElementsByClassName('numberOfItems'));
-
 numberOfItems.forEach((element) => {
     setNumberOfItems(element);
 });
-
 
 function setNumberOfItems(element) {
     const itemsOfShoppingCart = Array.from(document.getElementsByClassName('item'));
