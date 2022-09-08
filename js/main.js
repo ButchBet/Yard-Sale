154e9fe9f69f2bc1495e8514396ccf73b31aae6d
ButchBet <butchbet01@gmail.com>
2022-09-07 19:36:42 -0500

    Changed some styles and created script to add the number pf items added in the shopping cart in the shopping cart icon. I have created a global function in order to set the number of items each time that the user add a new one to the shopping cart

--- js/main.js
+++ js/main.js
@@ -17,3 +17,27 @@ const emailMobileMenu = document.getElementById('emailMobileMenu');
 emailMobileMenu.addEventListener('click', (e) => {
     e.preventDefault();
 })
+
+
+// Script to open and close the shopping cart
+const switchShoppingCarts = Array.from(document.getElementsByClassName('switchShoppingCart'));
+const shoppingCart = document.getElementById('shoppingCart');
+
+switchShoppingCarts.forEach((switchShoppingCart) => {
+    switchShoppingCart.addEventListener('click', (e) => {
+        shoppingCart.classList.toggle('hidden');
+    });
+})
+
+// Script to add the number pf items added in the shopping cart in the shopping cart icon
+const numberOfItems = Array.from(document.getElementsByClassName('numberOfItems'));
+
+numberOfItems.forEach((element) => {
+    setNumberOfItems(element);
+});
+
+
+function setNumberOfItems(element) {
+    const itemsOfShoppingCart = Array.from(document.getElementsByClassName('item'));
+    element.textContent = itemsOfShoppingCart.length.toString();
+}
