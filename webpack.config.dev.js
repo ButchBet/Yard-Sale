152621c0705037af331ea1dbaa827d2d2bbef2ab
Butch Bet <butchbet01@gmail.com>
2022-11-02 18:25:33 -0500

    Finished the general configurations.

---
+++ webpack.config.dev.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@styles': path.resolve(__dirname, 'src/styles')
+        },
+
+    },
+
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            },
+
+            {
+              test: /\.(s[ac]ss|css)$/,
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+              ]
+            }
+        ],
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+
+        new miniCssExtractPlugin({
+          filename: '[name].css',
+        }),
+    ],
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        // historyApiFallback: true,
+        port: 3006,
+        open: true,
+    } 
+}
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@styles': path.resolve(__dirname, 'src/styles')
+        },
+
+    },
+
+    mode: 'production',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            },
+
+            {
+              test: /\.(s[ac]ss|css)$/,
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+              ]
+            }
+        ],
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        })
+    ]
+}
