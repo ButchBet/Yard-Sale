02ee2f53b82df79f11c837d9b2282a28ed145128
Butch Bet <butchbet01@gmail.com>
2022-11-08 02:30:13 -0500

    Added an useSate in the item component, this in order to change the cart image when the user click on it.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "@styles/item.css";
 import cartIcon from "@images/icons/bt_add_to_cart.svg";
+import addedCartIcon from "@images/icons/bt_added_to_cart.svg";
 
 const Item = () => {
+    const [cart, setCart] = React.useState(false);
+
+    const handleCartClick = () => {
+        setCart(!cart);
+    }
     return(
         <div className="product-card">
             <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?cs=srgb&dl=pexels-pixabay-276517.jpg&fm=jpg" alt="product" className="product-img" />
@@ -14,8 +20,8 @@ const Item = () => {
                 </div>
                 
                 <div className="product-icon">
-                    <figure>
-                        <img src={cartIcon} alt="Add to card icon" />
+                    <figure onClick={handleCartClick}>
+                        <img src={!cart ? cartIcon : addedCartIcon} alt="Add to card icon" />
                     </figure>
                 </div>
             </div>
