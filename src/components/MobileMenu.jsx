832c949a122b8fe698b44dbd09b546d3b016099a
Butch Bet <butchbet01@gmail.com>
2023-01-16 13:08:10 -0500

    Removed the / from the routs or each link.

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -39,19 +39,19 @@ const MobileMenu = () => {
 
         <ul className="category-account">
             <li>
-                <a href="/my-orders">My orders</a>
+                <a href="my-orders">My orders</a>
             </li>
         
             <li>
-                <a href="/my-account">My account</a>
+                <a href="my-account">My account</a>
             </li>
 
             <li>
-                <a href="/my-account" className="email" id="emailMobileMenu">yardsale@gmail.com</a>
+                <a href="my-account" className="email" id="emailMobileMenu">yardsale@gmail.com</a>
             </li>
 
             <li>
-                <a href="/login" className="sing-out">sign out</a>
+                <a href="login" className="sing-out">sign out</a>
             </li>
         </ul>
     </div>
